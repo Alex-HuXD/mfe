1268b63df606f7f3c41b76dfe9750d30ae04e1dd
XinDi Hu <HuXDev@pm.me>
2023-06-16 16:27:54 -0500

    updates production webpack config for
    Marketing/Container

---
+++ container/config/webpack.prod.js
@@ -0,0 +1,27 @@
+const { merge } = require('webpack-merge')
+
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
+
+const commonConfig = require('./webpack.common')
+//delegating shared module management to webpack
+const packageJson = require('../package.json')
+
+const domain = process.env.PRODUCTION_DOMAIN
+
+const prodConfig = {
+    mode: 'production',
+    output: {
+        filename: '[name].[contenthash].js',
+    },
+    plugins: [
+        new ModuleFederationPlugin({
+            name: 'container',
+            remotes: {
+                marketingModule: `marketingModule@${domain}/marketing/remoteEntry.js`,
+            },
+            shared: packageJson.dependencies,
+        }),
+    ],
+}
+
+module.exports = merge(commonConfig, prodConfig)
